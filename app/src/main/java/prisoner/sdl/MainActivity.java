d264dd489bf2b9c078f75a969751ea389a464f33
Anssar Belgaid <anssar.belgaid@univ-tlse3.fr>
2024-03-22 11:23:15 +0100

    Page fin

--- app/src/main/java/prisoner/sdl/MainActivity.java
+++ app/src/main/java/prisoner/sdl/MainActivity.java
@@ -2,13 +2,31 @@ package prisoner.sdl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLaunchEndActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Trouver le bouton par son ID
+        btnLaunchEndActivity = findViewById(R.id.btnLaunchEndActivity);
+
+        // Ajouter un écouteur de clic au bouton
+        btnLaunchEndActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lancer endActivity
+                Intent intent = new Intent(MainActivity.this, EndActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
