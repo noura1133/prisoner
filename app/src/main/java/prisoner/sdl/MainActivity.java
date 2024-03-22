6bdca54855653846689b26881d5896811f9705e7
Mathis Vacher <vacher.mathis33@gmail.com>
2024-03-22 17:48:49 +0100

    test

--- app/src/main/java/prisoner/sdl/MainActivity.java
+++ app/src/main/java/prisoner/sdl/MainActivity.java
@@ -1,6 +1,7 @@
 package prisoner.sdl;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        // Initialiser la musique
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setLooping(false);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.seekTo(0);
+                mediaPlayer.start();
+            }
+        });
+        mediaPlayer.start();
+
+
+
+
+
+
 
     }
 }
