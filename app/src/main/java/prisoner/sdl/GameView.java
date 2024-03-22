fdc238cd977ae143c7270ed22d7f212382dbc8a8
Noura Ben Ali <noura1133@hotmail.fr>
2024-03-22 15:13:30 +0100

    Ajout du thread des buissons

---
+++ app/src/main/java/prisoner/sdl/GameView.java
@@ -0,0 +1,96 @@
+package prisoner.sdl;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private GameThread gameThread;
+    private Bitmap buissonBitmap;
+    private boolean buissonVisible;
+    private int buissonX, buissonY;
+
+    public GameView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        gameThread = new GameThread(getHolder(), this);
+        buissonVisible = false;
+        buissonBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bush);
+
+        int newWidth = buissonBitmap.getWidth() / 2; // Divisez la largeur actuelle par 2
+        int newHeight = buissonBitmap.getHeight() / 2; // Divisez la hauteur actuelle par 2
+        buissonBitmap = Bitmap.createScaledBitmap(buissonBitmap, newWidth, newHeight, false);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        gameThread.setRunning(true);
+        gameThread.start();
+        gameThread.startGame(); // Appel de la méthode startGame() pour initialiser le jeu
+
+    }
+
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        gameThread.setRunning(false);
+        while (retry) {
+            try {
+                gameThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                // En cas d'interruption, réessayer de fermer le thread
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (canvas != null) {
+            // Dessinez l'image du fond d'écran
+            Drawable background = ContextCompat.getDrawable(getContext(), R.drawable.fond_jeu);
+            if (background != null) {
+                background.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                background.draw(canvas);
+            }
+
+            // Dessinez le reste de votre contenu
+            if (buissonVisible) {
+                canvas.drawBitmap(buissonBitmap, buissonX, buissonY, null);
+            }
+        }
+    }
+
+    public void drawBuisson(Canvas canvas, int x, int y) {
+        // Dessinez le buisson aux coordonnées (x, y)
+        canvas.drawBitmap(buissonBitmap, x, y, null);
+    }
+
+
+    public void setBuissonPosition(int x, int y) {
+        buissonX = x;
+        buissonY = y;
+        buissonVisible = true;
+    }
+
+    public void hideBuisson() {
+        buissonVisible = false;
+    }
+
+
+}
